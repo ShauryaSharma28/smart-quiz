4be77bd458527e4bf8cf357e6d86b718750fe813
govindthakur20001 <govindthakur20001@gmail.com>
2025-09-25 15:24:48 +0530

    Defined core models and resolve environment setup

---
+++ quiz/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib import admin
+from .models import Quiz, Question, Answer, QuizAttempt
+
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuizAttempt)
